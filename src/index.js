f287a37bd2f8a1dde32498c9d579645895ccad6f
Ken Yamada <yamakenth@gmail.com>
2021-12-10 23:13:49 +0700

    Add comments for readability

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import './style.css';
 const content = document.querySelector('#content');
 
 const createHeader = (() => {
+  // header div 
   const header = document.createElement('div');
   header.classList.add('header');
-  
+  // top left logo 
   const logo = document.createElement('h1');
   logo.classList.add('logo');
   logo.textContent = 'LeCoffee Cafe';
-
+  // nav bar 
   const nav = document.createElement('div');
   nav.classList.add('nav');
   const home = document.createElement('h3');
@@ -22,17 +23,14 @@ const createHeader = (() => {
   contact.classList.add('contact-nav');
   contact.textContent = 'Contact';
   
-
-  
-  // test 
+  // append nav to nav bar 
   nav.appendChild(home);
   nav.appendChild(menu);
   nav.appendChild(contact);
-
+  // append elements to header 
   header.appendChild(logo);
   header.appendChild(nav);
-
+  // append header to page 
   content.appendChild(header);
-
 })();
 
