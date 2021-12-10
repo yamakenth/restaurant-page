79284007194074b1154abac1f178a173086404f7
Ken Yamada <yamakenth@gmail.com>
2021-12-10 22:37:39 +0700

    Add Webpage structure

---
+++ src/home.js
@@ -0,0 +1,15 @@
+const content = document.querySelector('#content');
+
+/*
+<h1>LeCoffee Cafe</h1>
+<h2>25 Years of Coffee and Community</h2>
+<img 
+  src="../src/coffee-cup-coffee-beans.png" 
+  alt="A coffee cup and a bag of coffee beans on a table"
+>
+<h3>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+  labore et dolore magna aliqua. Convallis aenean et tortor at risus. At auctor urna nunc id 
+  cursus metus aliquam eleifend mi.
+</h3>
+*/
--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import './style.css';
 
-console.log('Webpack is correctly set up.');
+const content = document.querySelector('#content');
+
+const createHeader = (() => {
+  const header = document.createElement('div');
+  header.classList.add('header');
+  
+  const logo = document.createElement('h1');
+  logo.classList.add('logo');
+  logo.textContent = 'LeCoffee Cafe';
+
+  const nav = document.createElement('div');
+  nav.classList.add('nav');
+  const home = document.createElement('h3');
+  home.classList.add('home-nav');
+  home.textContent = 'Home';
+  const menu = document.createElement('h3');
+  menu.classList.add('menu-nav');
+  menu.textContent = 'Menu';
+  const contact = document.createElement('h3');
+  contact.classList.add('contact-nav');
+  contact.textContent = 'Contact';
+  
+
+  
+  // test 
+  nav.appendChild(home);
+  nav.appendChild(menu);
+  nav.appendChild(contact);
+
+  header.appendChild(logo);
+  header.appendChild(nav);
+
+  content.appendChild(header);
+
+})();
+
