354a0b1efed5399bcce859a8d07d8b544cce4cd8
Ken Yamada <yamakenth@gmail.com>
2021-12-11 22:20:28 +0700

    Add ability to clear display section with nav buttons

--- src/index.js
+++ src/index.js
@@ -7,9 +7,37 @@ import './style.css';
 // when page load
 createTheme();
 const displayArea = document.querySelector('.display-area');
-// createHome(displayArea);
+createHome(displayArea);
+
+// when home button is clicked 
+const homeNav = document.querySelector('.home-nav');
+homeNav.addEventListener('click', () => {
+  clearDisplayArea();
+});
+
+// when menu button is clicked 
+const menuNav = document.querySelector('.menu-nav');
+menuNav.addEventListener('click', () => {
+  clearDisplayArea();
+});
+
+// when contact button is clicked 
+const contactNav = document.querySelector('.contact-nav');
+contactNav.addEventListener('click', () => {
+  clearDisplayArea();
+});
+
+
 
 // createMenu(displayArea);
-createContatct(displayArea);
+// createContatct(displayArea);
 
+function clearDisplayArea() {
+  console.log('click');
+  let child = displayArea.lastElementChild;
+  while (child) {
+    displayArea.removeChild(child);
+    child = displayArea.lastElementChild;
+  }
+}
 
